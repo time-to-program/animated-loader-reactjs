b15db61f1c51bcdceb122937f073090b9274bb96
raviteja <raviteja.venkatesh@costbo.com>
2022-07-05 18:20:41 +0530

    Create Animated Loader React JS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import AnimatedLoader from "./AnimatedLoader";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [imgList, setImgList] = useState([]);
+
+  const getAPIData = () => {
+    setLoading(true);
+
+    fetch("http://localhost:5000/getImages")
+      .then((response) => response.json())
+      .then((data) => {
+        setImgList(data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log("ERROR:", error);
+        setLoading(false);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {loading ? (
+        <AnimatedLoader />
+      ) : (
+        <div className="img-container">
+          {imgList.map((item, index) => {
+            return (
+              <img
+                src={item.imageURL}
+                width="200px"
+                height="100px"
+                style={{ objectFit: "cover" }}
+              />
+            );
+          })}
+        </div>
+      )}
+
+      <button className="btn" onClick={getAPIData}>
+        Get Data
+      </button>
     </div>
   );
 }
